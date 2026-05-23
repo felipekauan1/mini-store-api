e41febeb41a80c40c6ccf1fa936df8606803c20d
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-23 19:12:33 -0300

    feat: add categorias routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
+
+Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::post('/categorias', [CategoriaController::class, 'store']);
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::get('/produtos/{produto}', [ProdutoController::class, 'show']);
