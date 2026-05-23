962605d70d16b645363b323b6e9b21c133a5717b
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-23 18:39:43 -0300

    feat: add store, update and destroy routes for produtos

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
+Route::put('/produtos/{produto}', [ProdutoController::class, 'update']);
+Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy']);
