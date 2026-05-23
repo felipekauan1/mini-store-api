5af52b489e402b2860b8b072556608fff4d3a31f
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-23 18:51:26 -0300

    feat: add GET /api/produtos/{produto} route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::get('/produtos/{produto}', [ProdutoController::class, 'show']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::put('/produtos/{produto}', [ProdutoController::class, 'update']);
 Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy']);
