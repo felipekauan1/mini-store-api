632ae43c3b177e4aef80f186477ffc89715ddf3c
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-24 10:39:44 -0300

    feat: add clientes and pedidos routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
 
 Route::get('/categorias', [CategoriaController::class, 'index']);
 Route::post('/categorias', [CategoriaController::class, 'store']);
@@ -12,3 +14,11 @@ Route::get('/produtos/{produto}', [ProdutoController::class, 'show']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::put('/produtos/{produto}', [ProdutoController::class, 'update']);
 Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy']);
+
+Route::get('/clientes', [ClienteController::class, 'index']);
+Route::post('/clientes', [ClienteController::class, 'store']);
+Route::get('/clientes/{cliente}', [ClienteController::class, 'show']);
+
+Route::get('/pedidos', [PedidoController::class, 'index']);
+Route::post('/pedidos', [PedidoController::class, 'store']);
+Route::get('/pedidos/{pedido}', [PedidoController::class, 'show']);
