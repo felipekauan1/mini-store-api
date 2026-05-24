e149470b6811bc195a42c24535c275a2625d51b0
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-24 10:29:28 -0300

    feat: add PedidoController with index, store and show

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePedidoRequest;
+use App\Models\Pedido;
+
+class PedidoController extends Controller
+{
+    public function index()
+    {
+        $pedidos = Pedido::with('cliente')->get();
+
+        return response()->json([
+            'sucesso'  => true,
+            'total'    => $pedidos->count(),
+            'pedidos' => $pedidos,
+        ]);
+    }
+
+    public function store(StorePedidoRequest $request)
+    {
+        $pedido = Pedido::create(
+            $request->validated()
+        );
+
+        return response()->json([
+            'sucesso'  => true,
+            'mensagem' => 'Pedido cadastrado!',
+            'dados' => $pedido,
+        ], 201);
+    }
+
+    public function show(Pedido $pedido)
+    {
+        $pedido->load('cliente');
+
+        return response()->json([
+            'sucesso'  => true,
+            'pedido' => $pedido,
+        ]);
+    }
+}
